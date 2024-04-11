47a11b00afed46071073ab69802df2621e910f95
Mahdiiii4 <abod98451@gmail.com>
2024-04-11 13:59:55 +0200

    generiska klasser, metoder

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http.Headers;
+
+
+
+ Console.WriteLine(Min<int>(3,2));
+ Max<int>(2,5);
+T Min<T>(T val1, T val2) where T : IComparable<T>{
+
+    if(val1.CompareTo(val2) < 0)
+        return val1;
+    else 
+        return val2;
+}
+
+T Max<T>(T val1, T val2) where T : IComparable<T>{
+    return val2;
+}
